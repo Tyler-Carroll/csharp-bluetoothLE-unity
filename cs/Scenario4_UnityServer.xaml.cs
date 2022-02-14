ed41eb315e7ccc2a9b552c74b622d7269ea77e54
Tyler Carroll <htfu513@gmail.com>
2022-02-14 15:46:19 -0600

    Initial Commit

---
+++ cs/Scenario4_UnityServer.xaml.cs
@@ -0,0 +1,291 @@
+﻿using SDKTemplate;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Windows.ApplicationModel.Core;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace SDKTemplate
+{
+
+    public sealed partial class Scenario4_UnityServer : Page
+    {
+
+
+        // Every protocol typically has a standard port number. For example, HTTP is typically 80, FTP is 20 and 21, etc.
+        // For this example, we'll choose an arbitrary port number.
+        static string PortNumber = "5005";
+        public Scenario4_UnityServer()
+        {
+            this.InitializeComponent();
+        }
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.StartServer();
+        }
+        private async void StartServer()
+        {
+            try
+            {
+                var hostnames = NetworkInformation.GetHostNames();
+                var streamSocketListener = new Windows.Networking.Sockets.StreamSocketListener();
+
+                // The ConnectionReceived event is raised when connections are received.
+                streamSocketListener.ConnectionReceived += this.StreamSocketListener_ConnectionReceived;
+
+                // Start listening for incoming TCP connections on the specified port. You can specify any port that's not currently in use.
+                await streamSocketListener.BindEndpointAsync(new HostName("127.0.0.1"), PortNumber);
+
+                this.serverListBox.Items.Add($"Listening on {hostnames[2]}");
+            }
+            catch (Exception ex)
+            {
+                Windows.Networking.Sockets.SocketErrorStatus webErrorStatus = Windows.Networking.Sockets.SocketError.GetStatus(ex.GetBaseException().HResult);
+                this.serverListBox.Items.Add(webErrorStatus.ToString() != "Unknown" ? webErrorStatus.ToString() : ex.Message);
+            }
+        }
+
+        private async void StreamSocketListener_ConnectionReceived(Windows.Networking.Sockets.StreamSocketListener sender, Windows.Networking.Sockets.StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            string request;
+            using (var streamReader = new StreamReader(args.Socket.InputStream.AsStreamForRead()))
+            {
+                request = await streamReader.ReadLineAsync();
+            }
+
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add(string.Format("server received the request: \"{0}\"", request)));
+
+            // Echo the request back as the response.
+            using (Stream outputStream = args.Socket.OutputStream.AsStreamForWrite())
+            {
+                using (var streamWriter = new StreamWriter(outputStream))
+                {
+                    await streamWriter.WriteLineAsync(request);
+                    await streamWriter.FlushAsync();
+                }
+            }
+
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add(string.Format("server sent back the response: \"{0}\"", request)));
+
+            sender.Dispose();
+
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add("server closed its socket"));
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+/*
+
+namespace SDKTemplate
+{
+    public sealed partial class Scenario4_UnityServer : Page
+    {// A pointer back to the main page.  This is needed if you want to call methods in MainPage such
+        // as NotifyUser()
+        MainPage rootPage = MainPage.Current;
+
+        // List containing all available local HostName endpoints
+        private List<LocalHostItem> localHostItems = new List<LocalHostItem>();
+
+        public Scenario4_UnityServer()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+
+
+
+
+        /// <summary>
+        /// This is the click handler for the 'StartListener' button.
+        /// </summary>
+        /// <param name="sender">Object for which the event was generated.</param>
+        /// <param name="e">Event's parameters.</param>
+        private async void StartListener_Click(object sender, RoutedEventArgs e)
+        {
+            // Overriding the listener here is safe as it will be deleted once all references to it are gone.
+            // However, in many cases this is a dangerous pattern to override data semi-randomly (each time user
+            // clicked the button) so we block it here.
+            if (CoreApplication.Properties.ContainsKey("listener"))
+            {
+                rootPage.NotifyUser(
+                    "This step has already been executed. Please move to the next one.",
+                    NotifyType.ErrorMessage);
+                return;
+            }
+
+            if (String.IsNullOrEmpty(ServiceNameForListener.Text))
+            {
+                rootPage.NotifyUser("Please provide a port name.", NotifyType.ErrorMessage);
+                return;
+            }
+
+            CoreApplication.Properties.Remove("serverAddress");
+            CoreApplication.Properties.Remove("adapter");
+
+            LocalHostItem selectedLocalHost = null;
+
+            StreamSocketListener listener = new StreamSocketListener();
+            listener.ConnectionReceived += OnConnection;
+
+            // If necessary, tweak the listener's control options before carrying out the bind operation.
+            // These options will be automatically applied to the connected StreamSockets resulting from
+            // incoming connections (i.e., those passed as arguments to the ConnectionReceived event handler).
+            // Refer to the StreamSocketListenerControl class' MSDN documentation for the full list of control options.
+            listener.Control.KeepAlive = false;
+
+            // Save the socket, so subsequent steps can use it.
+            CoreApplication.Properties.Add("listener", listener);
+
+            // Start listen operation.
+            try
+            {
+
+                // Don't limit traffic to an address or an adapter.
+                await listener.BindServiceNameAsync(ServiceNameForListener.Text);
+                rootPage.NotifyUser("Listening", NotifyType.StatusMessage);
+
+            }
+            catch (Exception exception)
+            {
+                CoreApplication.Properties.Remove("listener");
+
+                // If this is an unknown status it means that the error is fatal and retry will likely fail.
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+
+                rootPage.NotifyUser(
+                    "Start listening failed with error: " + exception.Message,
+                    NotifyType.ErrorMessage);
+            }
+        }
+
+        /// <summary>
+        /// Invoked once a connection is accepted by StreamSocketListener.
+        /// </summary>
+        /// <param name="sender">The listener that accepted the connection.</param>
+        /// <param name="args">Parameters associated with the accepted connection.</param>
+        private async void OnConnection(
+            StreamSocketListener sender,
+            StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+
+            rootPage.NotifyUser($"Connected to: {sender}", NotifyType.StatusMessage);
+            DataReader reader = new DataReader(args.Socket.InputStream);
+            try
+            {
+                while (true)
+                {
+                    // Read first 4 bytes (length of the subsequent string).
+                    uint sizeFieldCount = await reader.LoadAsync(sizeof(uint));
+                    if (sizeFieldCount != sizeof(uint))
+                    {
+                        // The underlying socket was closed before we were able to read the whole data.
+                        return;
+                    }
+
+                    // Read the string.
+                    uint stringLength = reader.ReadUInt32();
+                    uint actualStringLength = await reader.LoadAsync(stringLength);
+                    if (stringLength != actualStringLength)
+                    {
+                        // The underlying socket was closed before we were able to read the whole data.
+                        return;
+                    }
+
+                    // Display the string on the screen. The event is invoked on a non-UI thread, so we need to marshal
+                    // the text back to the UI thread.
+                    NotifyUserFromAsyncThread(
+                        String.Format("Received data: \"{0}\"", reader.ReadString(actualStringLength)),
+                        NotifyType.StatusMessage);
+                }
+            }
+            catch (Exception exception)
+            {
+                // If this is an unknown status it means that the error is fatal and retry will likely fail.
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+
+                NotifyUserFromAsyncThread(
+                    "Read stream failed with error: " + exception.Message,
+                    NotifyType.ErrorMessage);
+            }
+        }
+
+        private void NotifyUserFromAsyncThread(string strMessage, NotifyType type)
+        {
+            var ignore = Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal, () => rootPage.NotifyUser(strMessage, type));
+        }
+
+
+
+
+        /// <summary>
+        /// Helper class describing a NetworkAdapter and its associated IP address
+        /// </summary>
+        class LocalHostItem
+        {
+            public string DisplayString
+            {
+                get;
+                private set;
+            }
+
+            public HostName LocalHost
+            {
+                get;
+                private set;
+            }
+
+            public LocalHostItem(HostName localHostName)
+            {
+                if (localHostName == null)
+                {
+                    throw new ArgumentNullException("localHostName");
+                }
+
+                if (localHostName.IPInformation == null)
+                {
+                    throw new ArgumentException("Adapter information not found");
+                }
+
+                this.LocalHost = localHostName;
+                this.DisplayString = "Address: " + localHostName.DisplayName +
+                    " Adapter: " + localHostName.IPInformation.NetworkAdapter.NetworkAdapterId;
+            }
+        }
+    }
+}
+*/
